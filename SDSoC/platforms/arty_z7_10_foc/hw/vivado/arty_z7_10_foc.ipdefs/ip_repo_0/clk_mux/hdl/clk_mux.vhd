82c1cdfd4a16af25ff7de217325467322e57c00e
Andrei Errapart <andrei.errapart@trenz.local>
2017-10-28 22:24:05 +0200

    platforms/arty_z7_10: Added license statements to the IP cores.

--- SDSoC/platforms/arty_z7_10_foc/hw/vivado/arty_z7_10_foc.ipdefs/ip_repo_0/clk_mux/hdl/clk_mux.vhd
+++ SDSoC/platforms/arty_z7_10_foc/hw/vivado/arty_z7_10_foc.ipdefs/ip_repo_0/clk_mux/hdl/clk_mux.vhd
@@ -1,22 +1,18 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- The 3-Clause BSD License
 -- 
--- Create Date: 19.09.2014 18:06:10
--- Design Name: 
--- Module Name: top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
+-- SPDX short identifier: BSD-3-Clause
 -- 
--- Dependencies: 
+-- Copyright 2016-2017 Trenz Electronic GmbH
 -- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 -- 
-----------------------------------------------------------------------------------
+-- 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+-- 
+-- 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+-- 
+-- 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 library IEEE;
